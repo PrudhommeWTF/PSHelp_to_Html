e713f96c698f2ce226ab21a71b012a6b8b7152c5
Thomas Prud'homme <5261600+PrudhommeWTF@users.noreply.github.com>
2019-09-26 17:49:44 +0200

    Sample and Landing page update

--- PSHelpToHtml.psm1
+++ PSHelpToHtml.psm1
@@ -406,7 +406,9 @@ Function New-ModuleHelpToHtml {
                     $MainContent += @"
     <div id="Landing" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
         <div class="row container-fluid">
-            $LandingPage
+            <div class="col-md-10">
+                $LandingPage
+            </div>
         </div>
     </div>
 
